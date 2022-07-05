995f5daec7ee8efbf9a99bdb934da59f4663fc92
lazlanrafar <lazlanrafar@gmail.com>
2022-07-05 19:51:15 +0700

    make model and migration lokasi

---
+++ database/migrations/2022_07_05_125014_create_lokasis_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lokasis', function (Blueprint $table) {
+            $table->id();
+            $table->string('namalokasi');
+            $table->string('unit');
+            $table->string('sublokasi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lokasis');
+    }
+};
