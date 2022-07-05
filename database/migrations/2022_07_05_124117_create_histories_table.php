db9f6201169990aa3d1c884f09b446142ea0b2f6
lazlanrafar <lazlanrafar@gmail.com>
2022-07-05 19:42:37 +0700

    make model and migration history

---
+++ database/migrations/2022_07_05_124117_create_histories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('histories', function (Blueprint $table) {
+            $table->id();
+            $table->string('uid');
+            $table->string('jenishistory');
+            $table->string('idprkt');
+            $table->string('kerusakan');
+            $table->string('perbaikan');
+            $table->date('tanggal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('histories');
+    }
+};
