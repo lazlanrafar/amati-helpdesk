f7247243248cabb1c1771e483f868e5b2a9e6a8b
lazlanrafar <lazlanrafar@gmail.com>
2022-07-05 19:52:52 +0700

    make model and migration SSID

---
+++ database/migrations/2022_07_05_125149_create_s_s_i_d_s_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('s_s_i_d_s', function (Blueprint $table) {
+            $table->id();
+            $table->integer('idlok');
+            $table->string('namassid');
+            $table->string('pwdssid');
+            $table->string('jenisssid');
+            $table->string('keterangan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('s_s_i_d_s');
+    }
+};
