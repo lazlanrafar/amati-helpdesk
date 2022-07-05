e27912d5815b0e28d58408162d8f841efd9e863d
lazlanrafar <lazlanrafar@gmail.com>
2022-07-05 19:46:59 +0700

    make model and migration access point

---
+++ database/migrations/2022_07_05_124414_create_access_points_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('access_points', function (Blueprint $table) {
+            $table->id();
+            $table->integer('idbrand');
+            $table->integer('idlok');
+            $table->string('jenis');
+            $table->integer('jumlahport');
+            $table->string('frekuensi');
+            $table->date('tgl_inventaris');
+            $table->string('keterangan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('access_points');
+    }
+};
