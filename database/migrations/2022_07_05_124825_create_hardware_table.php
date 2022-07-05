f7ff50cf5cbd2d7e9b91daf40e47a7250e8df942
lazlanrafar <lazlanrafar@gmail.com>
2022-07-05 19:50:05 +0700

    make model and migration hardware

---
+++ database/migrations/2022_07_05_124825_create_hardware_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hardware', function (Blueprint $table) {
+            $table->id();
+            $table->integer('idbrand');
+            $table->integer('idlok');
+            $table->string('jenis');
+            $table->string('koneksi');
+            $table->string('ipaddress');
+            $table->string('sharing');
+            $table->date('tgl_inventaris');
+            $table->string('keterangan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hardware');
+    }
+};
