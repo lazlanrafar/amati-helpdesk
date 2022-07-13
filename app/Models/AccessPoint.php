0178da0952577fdbdecda15f32c411591a67d690
lazlanrafar <lazlanrafar@gmail.com>
2022-07-13 14:58:46 +0700

    set model ap

--- app/Models/AccessPoint.php
+++ app/Models/AccessPoint.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class AccessPoint extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'idlok',
+        'idbrand',
+        'jenis_ap',
+        'jumlah_port',
+        'frekuensi',
+        'tgl_inventaris',
+        'keterangan',
+    ];
 }
