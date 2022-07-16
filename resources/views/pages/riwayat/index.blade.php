6a99734b4e5917d84815fd36ed26614c89ad3b12
lazlanrafar <lazlanrafar@gmail.com>
2022-07-16 08:57:31 +0700

    index history

--- resources/views/pages/riwayat/index.blade.php
+++ resources/views/pages/riwayat/index.blade.php
@@ -24,15 +24,68 @@
                             <table id="defaultTable" class="table table-bordered table-striped">
                                 <thead>
                                     <tr>
-                                        <th>Rendering engine</th>
-                                        <th>Browser</th>
-                                        <th>Platform(s)</th>
-                                        <th>Engine version</th>
-                                        <th>CSS grade</th>
+                                        <th>No</th>
+                                        <th>Jenis History</th>
+                                        <th>Perangkat</th>
+                                        <th>Kerusakan</th>
+                                        <th>Perbaikan</th>
+                                        <th>Tanggal</th>
+                                        <th>Aksi</th>
                                     </tr>
                                 </thead>
                                 <tbody>
+                                    <?php $i = 1; ?>
+                                    @forelse ($items as $item)
+                                        <tr>
+                                            <td>{{ $i }}</td>
+                                            <td>{{ $item->jenis_history }}</td>
+                                            <td>{{ $item->id_prkt }}</td>
+                                            <td>{{ $item->kerusakan }}</td>
+                                            <td>{{ $item->perbaikan }}</td>
+                                            <td>{{ $item->tanggal }}</td>
+                                            <td>
+                                                <form id="formDelete{{ $item->id }}"
+                                                    action="{{ route('lokasi.destroy', $item->id) }}" method="POST"
+                                                    class="d-inline">
+                                                    @csrf
+                                                    @method('delete')
+                                                    <a type="button" class="btn btn-danger"
+                                                        onclick="handleDelete({{ $item->id }})">
+                                                        <i class="fa fa-trash"></i>
+                                                    </a>
+                                                </form>
 
+                                                <script>
+                                                    function handleDelete(id) {
+                                                        Swal.fire({
+                                                            title: 'Are you sure?',
+                                                            text: "You won't be able to revert this!",
+                                                            icon: 'warning',
+                                                            showCancelButton: true,
+                                                            confirmButtonColor: '#3085d6',
+                                                            cancelButtonColor: '#d33',
+                                                            confirmButtonText: 'Yes, delete it!'
+                                                        }).then((result) => {
+                                                            if (result.isConfirmed) {
+                                                                document.getElementById('formDelete' + id).submit();
+                                                            }
+                                                        })
+                                                    }
+                                                </script>
+                                                <a type="button" class="btn btn-warning" data-toggle="modal"
+                                                    data-target="#formUpdate{{ $item->id }}">
+                                                    <i class="fa fa-edit"></i>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        <?php $i++; ?>
+                                    @empty
+                                        <tr>
+                                            <td colspan="7" class="text-center">
+                                                Data Kosong
+                                            </td>
+                                        </tr>
+                                    @endforelse
                                 </tbody>
                             </table>
                         </div>
