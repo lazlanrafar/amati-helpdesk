8b37eab140a99e264415e0e05eec11e10cc60fac
lazlanrafar <lazlanrafar@gmail.com>
2022-07-17 21:32:02 +0700

    index laporan

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -3,82 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\AccessPoint;
+use App\Models\Hardware;
+use App\Models\SwitchHub;
 
 class LaporanController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        return view('pages.laporan.index');
-    }
+        $list_ap = AccessPoint::join('brands', 'brands.id', '=', 'access_points.idbrand')
+            ->join('lokasis', 'lokasis.id', '=', 'access_points.idlok')
+            ->select('access_points.*', 'brands.nama_brand', 'brands.tipe_brand', 'lokasis.nama_lokasi', 'lokasis.unit', 'lokasis.sublokasi')
+            ->get();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+        $list_hardware = Hardware::join('brands', 'brands.id', '=', 'hardware.idbrand')
+            ->join('lokasis', 'lokasis.id', '=', 'hardware.idlok')
+            ->select('hardware.*', 'brands.nama_brand', 'brands.tipe_brand', 'lokasis.nama_lokasi', 'lokasis.unit', 'lokasis.sublokasi')
+            ->get();
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        $list_switch = SwitchHub::join('brands', 'brands.id', '=', 'switch_hubs.idbrand')
+            ->join('lokasis', 'lokasis.id', '=', 'switch_hubs.idlok')
+            ->select('switch_hubs.*', 'brands.nama_brand', 'brands.tipe_brand', 'lokasis.nama_lokasi', 'lokasis.unit', 'lokasis.sublokasi')
+            ->get();
+        
+        
+        return view('pages.laporan.index', [
+            'list_ap' => $list_ap,
+            'list_hardware' => $list_hardware,
+            'list_switch' => $list_switch
+        ]);
     }
 }
