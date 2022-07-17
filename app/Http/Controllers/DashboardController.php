b0d887a1d322ecf294c7141887471e74b2d08fb7
lazlanrafar <lazlanrafar@gmail.com>
2022-07-17 21:06:49 +0700

    dashboard row 1

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\AccessPoint;
+use App\Models\Hardware;
+use App\Models\SwitchHub;
 
 class DashboardController extends Controller
 {
@@ -14,6 +17,76 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('pages.dashboard.index');
+        $list_access_point = AccessPoint::join('lokasis', 'lokasis.id', '=', 'access_points.idlok')
+            ->select('access_points.*', 'lokasis.nama_lokasi')
+            ->get();
+        $list_hardware = Hardware::join('lokasis', 'lokasis.id', '=', 'hardware.idlok')
+            ->select('hardware.*', 'lokasis.nama_lokasi')
+            ->get();
+        $list_switch = SwitchHub::join('lokasis', 'lokasis.id', '=', 'switch_hubs.idlok')
+            ->select('switch_hubs.*', 'lokasis.nama_lokasi')
+            ->get();
+
+        $card_1_name = 'Kantor Korporat';
+        $card_1_total_access_point = $list_access_point->where('nama_lokasi', 'Kantor Korporat')->count();
+        $card_1_total_hardware = $list_hardware->where('nama_lokasi', 'Kantor Korporat')->count();
+        $card_1_total_switch = $list_switch->where('nama_lokasi', 'Kantor Korporat')->count();
+        $card_1_total_all = $card_1_total_access_point + $card_1_total_hardware + $card_1_total_switch;
+
+        $card_2_name = 'Unit Bisnis Service';
+        $card_2_total_access_point = $list_access_point->where('nama_lokasi', 'Unit Bisnis Service')->count();
+        $card_2_total_hardware = $list_hardware->where('nama_lokasi', 'Unit Bisnis Service')->count();
+        $card_2_total_switch = $list_switch->where('nama_lokasi', 'Unit Bisnis Service')->count();
+        $card_2_total_all = $card_2_total_access_point + $card_2_total_hardware + $card_2_total_switch;
+
+        $card_3_name = 'Unit Bisnis Infrastruktur';
+        $card_3_total_access_point = $list_access_point->where('nama_lokasi', 'Unit Bisnis Infrastruktur')->count();
+        $card_3_total_hardware = $list_hardware->where('nama_lokasi', 'Unit Bisnis Infrastruktur')->count();
+        $card_3_total_switch = $list_switch->where('nama_lokasi', 'Unit Bisnis Infrastruktur')->count();
+        $card_3_total_all = $card_3_total_access_point + $card_3_total_hardware + $card_3_total_switch;
+
+        $card_4_name = 'Unit Bisnis Gtrans';
+        $card_4_total_access_point = $list_access_point->where('nama_lokasi', 'Unit Bisnis Gtrans')->count();
+        $card_4_total_hardware = $list_hardware->where('nama_lokasi', 'Unit Bisnis Gtrans')->count();
+        $card_4_total_switch = $list_switch->where('nama_lokasi', 'Unit Bisnis Gtrans')->count();
+        $card_4_total_all = $card_4_total_access_point + $card_4_total_hardware + $card_4_total_switch;
+
+        $card_5_name = 'Unit Bisnis BES';
+        $card_5_total_access_point = $list_access_point->where('nama_lokasi', 'Unit Bisnis BES')->count();
+        $card_5_total_hardware = $list_hardware->where('nama_lokasi', 'Unit Bisnis BES')->count();
+        $card_5_total_switch = $list_switch->where('nama_lokasi', 'Unit Bisnis BES')->count();
+        $card_5_total_all = $card_5_total_access_point + $card_5_total_hardware + $card_5_total_switch;
+
+        return view('pages.dashboard.index', [
+            'card_1_name' => $card_1_name,
+            'card_1_total_access_point' => $card_1_total_access_point,
+            'card_1_total_hardware' => $card_1_total_hardware,
+            'card_1_total_switch' => $card_1_total_switch,
+            'card_1_total_all' => $card_1_total_all,
+
+            'card_2_name' => $card_2_name,
+            'card_2_total_access_point' => $card_2_total_access_point,
+            'card_2_total_hardware' => $card_2_total_hardware,
+            'card_2_total_switch' => $card_2_total_switch,
+            'card_2_total_all' => $card_2_total_all,
+
+            'card_3_name' => $card_3_name,
+            'card_3_total_access_point' => $card_3_total_access_point,
+            'card_3_total_hardware' => $card_3_total_hardware,
+            'card_3_total_switch' => $card_3_total_switch,
+            'card_3_total_all' => $card_3_total_all,
+
+            'card_4_name' => $card_4_name,
+            'card_4_total_access_point' => $card_4_total_access_point,
+            'card_4_total_hardware' => $card_4_total_hardware,
+            'card_4_total_switch' => $card_4_total_switch,
+            'card_4_total_all' => $card_4_total_all,
+
+            'card_5_name' => $card_5_name,
+            'card_5_total_access_point' => $card_5_total_access_point,
+            'card_5_total_hardware' => $card_5_total_hardware,
+            'card_5_total_switch' => $card_5_total_switch,
+            'card_5_total_all' => $card_5_total_all,
+        ]);
     }
 }
---
+++ resources/views/pages/dashboard/row-1.blade.php
@@ -0,0 +1,122 @@
+<div class="row justify-content-around">
+    <div class="col-lg-2 col-6">
+        <!-- small box -->
+        <div class="small-box bg-info">
+            <div class="inner">
+                <h3 class="mb-0">{{ $card_1_total_all }}</h3>
+                <p>{{ $card_1_name }}</p>
+                <hr>
+                <div class="row text-center">
+                    <div class="col-4">
+                        <h5 class="mb-0 font-weight-bold">{{ $card_1_total_hardware }}</h5>
+                        <p class="mb-0">Hardware</p>
+                    </div>
+                    <div class="col-4">
+                        <h5 class="mb-0 font-weight-bold">{{ $card_1_total_switch }}</h5>
+                        <p class="mb-0">Switch</p>
+                    </div>
+                    <div class="col-4">
+                        <h5 class="mb-0 font-weight-bold">{{ $card_1_total_access_point }}</h5>
+                        <p class="mb-0">AP</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-2 col-6">
+        <!-- small box -->
+        <div class="small-box bg-success">
+            <div class="inner">
+                <h3 class="mb-0">{{ $card_2_total_all }}</h3>
+                <p>{{ $card_2_name }}</p>
+                <hr>
+                <div class="row text-center">
+                    <div class="col-4">
+                        <h5 class="mb-0 font-weight-bold">{{ $card_2_total_hardware }}</h5>
+                        <p class="mb-0">Hardware</p>
+                    </div>
+                    <div class="col-4">
+                        <h5 class="mb-0 font-weight-bold">{{ $card_2_total_switch }}</h5>
+                        <p class="mb-0">Switch</p>
+                    </div>
+                    <div class="col-4">
+                        <h5 class="mb-0 font-weight-bold">{{ $card_2_total_access_point }}</h5>
+                        <p class="mb-0">AP</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-2 col-6">
+        <!-- small box -->
+        <div class="small-box bg-warning">
+            <div class="inner">
+                <h3 class="mb-0">{{ $card_3_total_all }}</h3>
+                <p>{{ $card_3_name }}</p>
+                <hr>
+                <div class="row text-center">
+                    <div class="col-4">
+                        <h5 class="mb-0 font-weight-bold">{{ $card_3_total_hardware }}</h5>
+                        <p class="mb-0">Hardware</p>
+                    </div>
+                    <div class="col-4">
+                        <h5 class="mb-0 font-weight-bold">{{ $card_3_total_switch }}</h5>
+                        <p class="mb-0">Switch</p>
+                    </div>
+                    <div class="col-4">
+                        <h5 class="mb-0 font-weight-bold">{{ $card_3_total_access_point }}</h5>
+                        <p class="mb-0">AP</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-2 col-6">
+        <!-- small box -->
+        <div class="small-box bg-primary">
+            <div class="inner">
+                <h3 class="mb-0">{{ $card_4_total_all }}</h3>
+                <p>{{ $card_4_name }}</p>
+                <hr>
+                <div class="row text-center">
+                    <div class="col-4">
+                        <h5 class="mb-0 font-weight-bold">{{ $card_4_total_hardware }}</h5>
+                        <p class="mb-0">Hardware</p>
+                    </div>
+                    <div class="col-4">
+                        <h5 class="mb-0 font-weight-bold">{{ $card_4_total_switch }}</h5>
+                        <p class="mb-0">Switch</p>
+                    </div>
+                    <div class="col-4">
+                        <h5 class="mb-0 font-weight-bold">{{ $card_4_total_access_point }}</h5>
+                        <p class="mb-0">AP</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-2 col-6">
+        <!-- small box -->
+        <div class="small-box bg-danger">
+            <div class="inner">
+                <h3 class="mb-0">{{ $card_5_total_all }}</h3>
+                <p>{{ $card_5_name }}</p>
+                <hr>
+                <div class="row text-center">
+                    <div class="col-4">
+                        <h5 class="mb-0 font-weight-bold">{{ $card_5_total_hardware }}</h5>
+                        <p class="mb-0">Hardware</p>
+                    </div>
+                    <div class="col-4">
+                        <h5 class="mb-0 font-weight-bold">{{ $card_5_total_switch }}</h5>
+                        <p class="mb-0">Switch</p>
+                    </div>
+                    <div class="col-4">
+                        <h5 class="mb-0 font-weight-bold">{{ $card_5_total_access_point }}</h5>
+                        <p class="mb-0">AP</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
