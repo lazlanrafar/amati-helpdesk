e2910f653b8defe76e5c8572fc0a688cb251877b
lazlanrafar <lazlanrafar@gmail.com>
2022-07-22 09:32:25 +0700

    set footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,10 +1,7 @@
 <footer class="main-footer">
-    <strong
-        >Copyright &copy; 2014-2021
-        <a href="https://adminlte.io">AdminLTE.io</a>.</strong
-    >
+    <strong>Copyright &copy; 2022 -
+        Kevin Kusuma Wicaksono</strong>
     All rights reserved.
-    <div class="float-right d-none d-sm-inline-block"><b>Version</b> 3.2.0</div>
 </footer>
 
 <!-- Control Sidebar -->
